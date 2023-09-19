bf373f831fdaf38e36f84d863d5509f60669befd
Satwik Jain <125976297+satwikjain23@users.noreply.github.com>
2023-09-19 20:27:15 +0530

    Update dist_finder.py

--- f1tenth-course-labs/race/src/dist_finder.py
+++ f1tenth-course-labs/race/src/dist_finder.py
@@ -7,10 +7,10 @@ from sensor_msgs.msg import LaserScan
 from race.msg import pid_input
 
 # Some useful variable declarations.
-angle_range = 240	# Hokuyo 4LX has 240 degrees FoV for scan
-forward_projection = 0.5	# distance (in m) that we project the car forward for correcting the error. You have to adjust this.(1.5)(0.5)
-desired_distance = 0.6	# distance from the wall (in m). (defaults to right wall)(0.9)
-vel = 15 		# this vel variable is not really used here.
+angle_range = 	# Hokuyo 4LX has 240 degrees FoV for scan
+forward_projection = 	# distance (in m) that we project the car forward for correcting the error. You have to adjust this.(1.5)(0.5)
+desired_distance = 	# distance from the wall (in m). (defaults to right wall)(0.9)
+vel = 1.0		# this vel variable is not really used here.
 error = 0.0		# initialize the error
 car_length = 0.50 # Traxxas Rally is 20 inches or 0.5 meters
 c=0.6
@@ -26,40 +26,23 @@ def getRange(data,angle):
     # Outputs length in meters to object with angle in lidar scan field of view
     # Make sure to take care of NaNs etc.
     #TODO: implement
-	index = (angle)*(len(data.ranges)/240)
-	index = int(index)
 	
-	
-	return data.ranges[index]
-
 
 
 def callback(data):
 	global forward_projection
 
-	theta = 70 # you need to try different values for theta(70)
-	a = getRange(data,theta) # obtain the ray distance for theta
-	b = getRange(data,30)	# obtain the ray distance for 0 degrees (i.e. directly to the right of the car)
-	c = getRange(data,120)
+	theta =   # you need to try different values for theta
+         # obtain the ray distance for theta
+	 # obtain the ray distance for 0 degrees (i.e. directly to the right of the car)
 	
-	swing = math.radians(theta)
-    
-	if c>7:
-		if (a>1.2):
-			a=0.9
-		if (b >2):
-			b=1.3
 	
 	
 
 	## Your code goes here to determine the error as per the alrorithm 
 	# Compute Alpha, AB, and CD..and finally the error.
 	# TODO: implement
-	Alpha = math.atan((a*(math.cos(swing)) - b)/a*math.sin(swing))
-	AB = b*math.cos(Alpha)
-	CD = AB + forward_projection*math.sin(Alpha)
-	error = CD - desired_distance
-	print("Alpha=",math.degrees(Alpha))
+	
 	msg = pid_input()	# An empty msg is created of the type pid_input
 	# this is the error that you want to send to the PID for steering correction.
 	msg.pid_error = error	
